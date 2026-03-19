17aedcdac2a061ecb6a7029622a65de4a23b9498
faal <130977909+faal2@users.noreply.github.com>
2026-03-19 08:50:16 +0100

    API endpoint

---
+++ LMS.Presentation/Controllers/ActivitiesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS.Presentation.Controllers
+{
+    [Route("api/modules/{moduleId}/activities")]
+    [ApiController]
+    //[Authorize] 
+    public class ActivitiesController : ControllerBase
+    {
+        private readonly IActivityService _activityService;
+
+        public ActivitiesController(IActivityService activityService)
+        {
+            _activityService = activityService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetActivitiesForModule(Guid moduleId)
+        {
+            var activities = await _activityService.GetActivitiesForModuleAsync(moduleId);
+            return Ok(activities);
+        }
+    }
+}

c9e2db6e0106ef5d2fbf614fae7460c51ed187e6
faal <130977909+faal2@users.noreply.github.com>
2026-03-19 08:52:06 +0100

    contracts for activity handling

---
+++ Service.Contracts/IActivityService.cs
@@ -0,0 +1,12 @@
+﻿using LMS.Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Contracts
+{
+    public interface IActivityService
+    {
+        Task<IEnumerable<ActivityDto>> GetActivitiesForModuleAsync(Guid moduleId);
+    }
+}

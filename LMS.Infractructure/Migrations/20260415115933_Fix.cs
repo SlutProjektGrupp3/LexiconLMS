9e8f1130d5b1f0062b7b47a9ce212c6b86400cd4
RakanEngineer <rakan.engineer@gmail.com>
2026-04-15 14:10:53 +0200

    Fix Bug när man tar bort någon kurs som har elev

---
+++ LMS.Infractructure/Migrations/20260415115933_Fix.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS.Infractructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Activities_ActivityTypes_TypeId",
+                table: "Activities");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Courses_CourseId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Activities_ActivityTypes_TypeId",
+                table: "Activities",
+                column: "TypeId",
+                principalTable: "ActivityTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Courses_CourseId",
+                table: "AspNetUsers",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Activities_ActivityTypes_TypeId",
+                table: "Activities");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Courses_CourseId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Activities_ActivityTypes_TypeId",
+                table: "Activities",
+                column: "TypeId",
+                principalTable: "ActivityTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Courses_CourseId",
+                table: "AspNetUsers",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id");
+        }
+    }
+}
